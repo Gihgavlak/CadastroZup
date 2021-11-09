26faa0096603357d388358b24d4a9774ef92a48a
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-11-08 22:36:28 -0300

    Trocar extends para JPA para usar @Query

--- src/main/java/br/com/zup/Cadastros/cadastro/CadastroRepository.java
+++ src/main/java/br/com/zup/Cadastros/cadastro/CadastroRepository.java
@@ -1,6 +1,14 @@
 package br.com.zup.Cadastros.cadastro;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface CadastroRepository extends CrudRepository<Cadastro, String> {
+import java.util.List;
+
+public interface CadastroRepository extends JpaRepository<Cadastro, String> {
+    @Query( value = "select * from cadastros where mora_sozinho=true",nativeQuery = true)
+    public List<Cadastro> exibirListaMoraSozinho ();
+
+    @Query( value = "select * from cadastros where tem_pet=true",nativeQuery = true)
+    public List<Cadastro> exibirListaTemPet ();
 }
